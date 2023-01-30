df1eaaca062c9a2f53f4c7dc0184ea0f889dac39
Preeti-ojha <ojhapreeti075@gmail.com>
2023-01-30 11:56:50 +0530

    added implementation for diff

--- Diff.java
+++ Diff.java
@@ -3,6 +3,7 @@ public class Diff{
     publis static void main(){
         int a=5;
         int b = 7 ;
+        System.out.print("small change");
         System.out.print("The Diff is "+(a-b));
     }
 }
